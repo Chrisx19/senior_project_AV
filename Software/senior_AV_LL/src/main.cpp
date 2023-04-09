8debef1d853f6294c3103711b0de9618b46599eb
Chrisx19 <Christopherlazo1619@gmail.com>
2023-04-09 15:03:58 -0700

    Convert QPPS -> m/s

--- Software/senior_AV_LL/src/main.cpp
+++ Software/senior_AV_LL/src/main.cpp
@@ -1,7 +1,7 @@
 #include <ros.h>
 #include <geometry_msgs/Twist.h>
-#include <std_msgs/UInt32.h>
-#include <std_msgs/UInt8.h>
+#include <std_msgs/Int32.h>
+#include <std_msgs/Float32.h>
 #include <SoftwareSerial.h>
 #include "RoboClaw.h"
 #include <math.h>
@@ -10,8 +10,9 @@
 #define address2 0x81
 #define address3 0x82     //corner_motor
 
+#define wheel_radius 0.076
 #define ppr_motor 537.7
-#define wheel_circumference 2*PI*0.076
+#define circumference (2 * PI * wheel_radius)
 
 ros::NodeHandle  nh;
 
@@ -19,25 +20,25 @@ SoftwareSerial serial(10, 11);     //serial (rx, tx)
 RoboClaw roboclaw(&serial, 10000); //10000
 //==========================================================================Global================================================================================
 void AV_vel_cb(const geometry_msgs::Twist& AV_vel_msg);
-void forward_backward(int val);
 void right_left(int turn);
 void motor_stop(void);
-uint32_t avg_encoder_val(int addy, int addy_2);
-uint8_t meter_per_sec(int qpps);
+int avg_encoder_val(int addy, int addy_2);
 
-int g_av_encoder = avg_encoder_val(address1, address2);
+int g_av_encoder = 0;
+float g_m_s = 0;
 
 ros::Subscriber<geometry_msgs::Twist> AV_vel_sub("cmd_vel_AV", &AV_vel_cb);
 
-std_msgs::UInt32 enc;
-ros::Publisher enc_pub("encoder_wheel", &enc);
+std_msgs::Int32 enc;
+ros::Publisher enc_pub("qpps", &enc);
 
-std_msgs::UInt8 speed;
+std_msgs::Float32 speed;
 ros::Publisher speed_pub("speed_av", &speed);
-//==========================================================================Steup and Loop================================================================================
+//==========================================================================Setup and Loop================================================================================
 void setup()
 {
   roboclaw.begin(115200);
+  nh.getHardware()->setBaud(115200);
 
   nh.initNode();
   nh.advertise(enc_pub);
@@ -51,10 +52,10 @@ void loop()
   enc.data = g_av_encoder;
   enc_pub.publish( &enc);
 
-  speed.data = meter_per_sec(g_av_encoder);
+  speed.data = g_m_s;
   speed_pub.publish( &speed);
   nh.spinOnce();
-//  delay(1);
+  // delay(1);
 }
 //==========================================================================Functions================================================================================
 void AV_vel_cb(const geometry_msgs::Twist& AV_vel_msg)  //callback function from subscribe
@@ -62,42 +63,24 @@ void AV_vel_cb(const geometry_msgs::Twist& AV_vel_msg)  //callback function from
   int drive_duty_cycle = AV_vel_msg.linear.x;           //val: -63 <-> 63
   int corner = AV_vel_msg.angular.z + 780;
 
-  forward_backward(drive_duty_cycle);
+  g_av_encoder = avg_encoder_val(address1, address2);
+  // g_av_encoder = roboclaw.ReadSpeedM2(address1);
+  g_m_s = circumference * (1 / ppr_motor) * g_av_encoder;
+
+  roboclaw.SpeedM1M2(address1, drive_duty_cycle, drive_duty_cycle);
+  roboclaw.SpeedM1M2(address2, drive_duty_cycle, drive_duty_cycle);
   right_left(corner);
 }//end fo AV_vel_cb
 
-uint32_t avg_encoder_val(int addy, int addy_2)
+int avg_encoder_val(int addy, int addy_2)
 {
-  uint32_t encoder1_M1 = roboclaw.ReadEncM1(addy);
-  uint32_t encoder2_M1 = roboclaw.ReadEncM2(addy);
-  uint32_t encoder1_M2 = roboclaw.ReadEncM1(addy_2);
-  uint32_t encoder2_M2 = roboclaw.ReadEncM2(addy_2);
+  int encoder1_M1 = roboclaw.ReadSpeedM1(addy);
+  int encoder2_M1 = roboclaw.ReadSpeedM2(addy);
+  int encoder1_M2 = roboclaw.ReadSpeedM1(addy_2);
+  int encoder2_M2 = roboclaw.ReadSpeedM2(addy_2);
 
   return (encoder1_M1 + encoder2_M1 + encoder1_M2 + encoder2_M2) / 4;
 }//end of avg_encoder_val
-
-uint8_t meter_per_sec(int qpps)
-{
-  return wheel_circumference * (1/ppr_motor) * qpps;
-}
-
-void forward_backward(int drive)
-{
-  if(drive > 0)
-  {
-    roboclaw.ForwardM1(address1, drive);
-    roboclaw.ForwardM2(address1, drive);
-    roboclaw.ForwardM1(address2, drive);
-    roboclaw.ForwardM2(address2, drive);
-  }
-  else
-  {
-    roboclaw.BackwardM1(address1, drive * -1);
-    roboclaw.BackwardM2(address1, drive * -1);
-    roboclaw.BackwardM1(address2, drive * -1);
-    roboclaw.BackwardM2(address2, drive * -1);
-  }
-}//end of forward_backward
 
 void right_left(int turn)
 {
@@ -127,6 +110,6 @@ void motor_stop(void)
   uint8_t rc[3] = {address1, address2, address3};
   for(int i = 0; i < 3; i++)
   {
-    roboclaw.ForwardM1(rc[i], 0);
+    roboclaw.SpeedM1M2(rc[i], 0, 0);
   }
 }//end of motor_stop
