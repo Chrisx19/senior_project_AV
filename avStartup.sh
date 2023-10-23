68197672d3ab39a066ee3ff3f8e7a4ed42ce7154
Chris Lazo <107272321+Chrisx19@users.noreply.github.com>
2023-10-22 17:27:41 -0700

    Update avStartup.sh

--- avStartup.sh
+++ avStartup.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-# Source your ROS setup file
-#source /opt/ros/noetic/setup.bash  # Replace <ROS-DISTRO> with your ROS distribution
-
-# Navigate to your ROS workspace if needed
-# cd /path/to/your/ros/workspace
-
 # Run your ROS launch file
 bash -c ". /home/jetson/rover_ws/devel/setup.bash"
 bash -c ". /opt/ros/noetic/setup.bash"
